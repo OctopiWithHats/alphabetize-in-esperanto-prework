ac86802fe17fd48fc02540edae99f41a66219ffb
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-23 16:56:48 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,6 +3,6 @@ def alphabetize(arr)
   
   esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
   
-  
+  arr.sort_by 
   
 end
