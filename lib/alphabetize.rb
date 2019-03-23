48a4caed3de346f2d5198b972d777a1dae6f3fad
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-23 16:54:15 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,12 +1,6 @@
 def alphabetize(arr)
   # code here
   
-  arr.sort_by! do |a|
-    positions = []
-    a.each_char do |char|
-      positions << ALPHABET.index(char)
-    end
-    positions
-  end
-  arr
+  esperanto_alphabet = 
+  
 end
