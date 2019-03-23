27b3c95341023a165c11766da927730db76f5ecf
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-23 17:37:01 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,13 @@
+ESPERANTO_ALPHABET
+
 def alphabetize(arr)
   # code here
-  esperanto = ["abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"]
-  alphabet = esperanto.split("")
-  
-  
+  arr.sort_by! do |a|
+    positions = []
+    a.each_char do |char|
+      positions << ESPERANTO_ALPHABET.index(char)
+    end
+    positions
+  end
+  arr
 end
