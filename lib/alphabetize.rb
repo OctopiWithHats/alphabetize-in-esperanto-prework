5aaa935779cc5a67ae2e752a938ce4fc98de90aa
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-23 16:52:41 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,7 +4,7 @@ def alphabetize(arr)
   arr.sort_by! do |a|
     positions = []
     a.each_char do |char|
-      positions << ESPERANTO_ALPHABET.index(char)
+      positions << ALPHABET.index(char)
     end
     positions
   end
